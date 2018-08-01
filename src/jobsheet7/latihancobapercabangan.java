9730cf5c821d7a21296709dd40cee155eb337048
Client <Client@DESKTOP-D1LV6HD>
2018-08-01 09:34:44 +0700

    Coba IF

--- src/jobsheet7/latihancobapercabangan.java
+++ src/jobsheet7/latihancobapercabangan.java
@@ -16,5 +16,19 @@ public class latihancobapercabangan {
         String identitas = "baliyun / X RPL 5 / 23";
         System.out.println("Identitas : "+ identitas);
         
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nilai a ? ");
+        int a = scanner.nextInt();
+        
+        System.out.println("\nIF.. 1");
+        if(a < 5) System.out.println("nilai a kurang dari 5");
+        
+        System.out.println("\nIF.. 2");
+        if(a == 5) System.out.println("nilai a sama dengan 5");
+        
+        System.out.println("\nIF.. 3");
+        if(a > 5) { 
+            System.out.println("isi variabel a : "+ a);
+            System.out.println("nilai a lebih dari 5");
     }
 }
