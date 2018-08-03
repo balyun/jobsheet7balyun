e22df07de0a6fa1b77851bb069844ca15ec8e4db
Client <Client@DESKTOP-D1LV6HD>
2018-08-03 11:20:29 +0700

    Coba IF

---
+++ src/latihan/latihancobapercabangan.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Client
+ */
+public class latihancobapercabangan {
+    public static void main(String[] args) {
+        String identitas = "baliyun / X RPL 5 / 23";
+        System.out.println("Identitas : "+ identitas);
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nilai a ? ");
+        int a = scanner.nextInt();
+        
+        System.out.println("\nIF.. 1");
+        if(a < 5) System.out.println("nilai a kurang dari 5");
+        
+        System.out.println("\nIF.. 2");
+        if(a == 5) System.out.println("nilai a sama dengan 5");
+        
+        System.out.println("\nIF.. 3");
+        if(a > 5) { 
+            System.out.println("isi variabel a : "+ a);
+            System.out.println("nilai a lebih dari 5");
+            
+        }
+    }
+}    
