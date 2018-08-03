d2bb87a6153768464bb8bb923b8f49eeedc80374
Client <Client@DESKTOP-D1LV6HD>
2018-08-03 13:00:36 +0700

    Initial Commit

---
+++ src/latihan/latihancobapercabangan2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan;
+
+/**
+ *
+ * @author Client
+ */
+public class latihancobapercabangan2 {
+    public static void main(String[] args) {
+    
+    String identitas = "Mohammad Baliyun Taja / X RPL 10 / 23";
+    System.out.println("Identitas : "+ identitas);
+    
+    System.out.print("\nSaran Resep dari Bahan Milik Anda\n");
+    
+    } 
+}
